9226efa0e57b02e1ea58bdffbf10a836eed6f6a9
Karan <xxkguyxxdev@gmail.com>
2023-10-17 22:42:17 -0400

    More yeet

--- src/main/java/co/yorku/nutrifit/exercise/Exercise.java
+++ src/main/java/co/yorku/nutrifit/exercise/Exercise.java
@@ -1,11 +1,9 @@
 package co.yorku.nutrifit.exercise;
-import co.yorku.nutrifit.event.IEventListener;
+
 
 //This is the interface for the exercise class
 //The purpose of this interface is to handle interaction
 //related to the user logging of exercise data
-public interface Exercise extends IEventListener {
-
-
+public interface Exercise {
 
 }
