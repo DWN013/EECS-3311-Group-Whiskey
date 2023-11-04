b68730cf55e3270511271efc430545d2e541d399
Karan <xxkguyxxdev@hotmail.com>
2023-11-04 00:38:24 -0400

    Some database stuff

--- src/main/java/co/yorku/nutrifit/database/nutrient/impl/NFDatabase.java
+++ src/main/java/co/yorku/nutrifit/database/nutrient/impl/NFDatabase.java
@@ -3,11 +3,38 @@ package co.yorku.nutrifit.database.nutrient.impl;
 import co.yorku.nutrifit.database.nutrient.INFDatabase;
 import co.yorku.nutrifit.object.NutrientInfo;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class NFDatabase implements INFDatabase {
+    private Connection databaseConnection;
+
+    public NFDatabase() {
+        File dbFile = new File("nfdatabase.db");
+        if (!dbFile.exists()) {
+            try {
+                dbFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.databaseConnection = DriverManager.getConnection("jdbc:sqlite:" + dbFile.getAbsolutePath());
+            System.out.println("Established connection with sqlite database. [NFDatabase]");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
-    public void setupDatabase() {
-        // TODO
+    public boolean setupDatabase() {
+
+        return true; // tmp
     }
 
     @Override
@@ -18,6 +45,12 @@ public class NFDatabase implements INFDatabase {
 
     @Override
     public void close() {
-
+        if (this.databaseConnection != null) {
+            try {
+                this.databaseConnection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
