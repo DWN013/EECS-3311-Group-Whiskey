23aff61b2075920df2ca93b437321233450535c6
Alexander Ukhin <alexanderukhin@gmail.com>
2023-11-07 18:33:10 -0500

    Oops! All math!

--- src/main/java/co/yorku/nutrifit/exercise/calculators/ActivityRatingCalculator.java
+++ src/main/java/co/yorku/nutrifit/exercise/calculators/ActivityRatingCalculator.java
@@ -1,15 +1,48 @@
 package co.yorku.nutrifit.exercise.calculators;
 
+import co.yorku.nutrifit.NutriFit;
 import co.yorku.nutrifit.exercise.IExercise;
+import co.yorku.nutrifit.logs.LogIterator;
+import co.yorku.nutrifit.logs.impl.Exercise;
+import co.yorku.nutrifit.logs.impl.Meal;
+import co.yorku.nutrifit.profile.IProfile;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class ActivityRatingCalculator {
 
     private IExercise exercise;
-
-    public ActivityRatingCalculator(IExercise exercise) {
-        this.exercise = exercise;
-    }
-
     // This class calculates how active am I
+    public double getUserActivityRating(){
+        //Gets the logs for 14 days worth of exercises
+        LogIterator ex_iterator = NutriFit.getInstance().getUserDatabase().getUserExerciseLogs(
+                    NutriFit.getInstance().getLoadedProfile().getId(),
+                    new Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(14)),
+                    new Date(System.currentTimeMillis())
+            );
+            //Sort data from oldest to newest log
+            ex_iterator.sortByDateAscending();
+            int totalExerciseTime = 0;
 
+                while (ex_iterator.hasNext()){
+                Exercise exercise = (Exercise) ex_iterator.getNext();
+                if (exercise != null) {
+                    // Add exercise duration to total exercise time in minutes
+                    totalExerciseTime += ((exercise.getTimeSpentInSeconds()) / 60);
+                }
+            }
+            double averageExerciseTimePerWeek = ((double) totalExerciseTime)/14;
+                if (averageExerciseTimePerWeek <= 0.0) {
+                return 1.2;
+            } else if (averageExerciseTimePerWeek > 0.0 && averageExerciseTimePerWeek <= 180.0) {
+                return 1.375;
+            } else if (averageExerciseTimePerWeek > 180.0 && averageExerciseTimePerWeek <= 300.0) {
+                return 1.55;
+            } else if (averageExerciseTimePerWeek > 300.0 && averageExerciseTimePerWeek <= 420.0) {
+                return 1.725;
+            } else {
+                return 1.9;
+            }
+    }
 }
--- src/main/java/co/yorku/nutrifit/exercise/calculators/WeightLossCalculator.java
+++ src/main/java/co/yorku/nutrifit/exercise/calculators/WeightLossCalculator.java
@@ -5,59 +5,38 @@ import co.yorku.nutrifit.logs.LogIterator;
 import co.yorku.nutrifit.logs.impl.Exercise;
 import co.yorku.nutrifit.logs.impl.Meal;
 import co.yorku.nutrifit.profile.IProfile;
+import co.yorku.nutrifit.visualizer.calulcators.DailyTotalEnergyExpenditureCalculator;
 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class WeightLossCalculator {
+    private DailyTotalEnergyExpenditureCalculator dailyTotalExpend;
     // Need to know how many calories I burn living (figure out how activate user is)
+    // Weightloss calc uses BMR and Activity rating calc and multiplies em,
     // RMR == Resting Metabolic Rate
-    public double getUserRMR(IProfile profile){
-        LogIterator ex_iterator = NutriFit.getInstance().getUserDatabase().getUserExerciseLogs(
-                NutriFit.getInstance().getLoadedProfile().getId(),
-                new Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(14)),
-                new Date(System.currentTimeMillis())
-        );
+    public void getUserRMR(IProfile profile){
         LogIterator diet_iterator = NutriFit.getInstance().getUserDatabase().getUserMealLogs(
                 NutriFit.getInstance().getLoadedProfile().getId(),
                 new Date(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(14)),
                 new Date(System.currentTimeMillis())
         );
         // Sorts by oldest to newest dates
-        ex_iterator.sortByDateAscending();
         diet_iterator.sortByDateAscending();
 
         // While there is still a log next in the linked list for both meal and exercise, go to the next log
         //correct this, people don't need to exercise every day but they do need to eat
-        int totalExerciseTime = 0;
         int totalCalorieIntake = 0;
-        while (ex_iterator.hasNext() || diet_iterator.hasNext()){
-            Exercise exercise = (Exercise) ex_iterator.getNext();
+        while (diet_iterator.hasNext()){
             Meal diet = (Meal) diet_iterator.getNext();
-            if (exercise != null) {
-                // Add exercise duration to total exercise time
-                totalExerciseTime += (exercise.getTimeSpentInSeconds())/60;
-            }
-
             if (diet != null) {
                 // Add meal calorie intake to total calorie intake
                 totalCalorieIntake += diet.getTotalMealCalories();
             }
         }
-        //End of aggregation of exercise and meal info
-        //------------------------------------------------------------------------------------
-        double averageExerciseTimePerWeek = ((double) totalExerciseTime)/14;
-        if (averageExerciseTimePerWeek <= 0.0) {
-            return 1.2;
-        } else if (averageExerciseTimePerWeek > 0.0 && averageExerciseTimePerWeek <= 180.0) {
-            return 1.375;
-        } else if (averageExerciseTimePerWeek > 180.0 && averageExerciseTimePerWeek <= 300.0) {
-            return 1.55;
-        } else if (averageExerciseTimePerWeek > 300.0 && averageExerciseTimePerWeek <= 420.0) {
-            return 1.725;
-        } else {
-            return 1.9;
-        }
+        //Average amount of calories eaten per day.
+        double totalAverageCalorieIntake = ((double) totalCalorieIntake)/14;
+        double caloriesExpendedPerDay = dailyTotalExpend.getDTEE();
 
     }
 }
