d93d7c926d643d8b1926efba10833a9e7cda25a2
Pranav Kulkarni <121322862+PranavKulkarni33@users.noreply.github.com>
2023-11-14 19:50:40 -0500

    Changes to CFG

--- src/main/java/co/yorku/nutrifit/visualizer/impl/CFGPlateVisualizer.java
+++ src/main/java/co/yorku/nutrifit/visualizer/impl/CFGPlateVisualizer.java
@@ -42,32 +42,11 @@ public class CFGPlateVisualizer extends IVisualizer {
 
     @Override
     public DefaultCategoryDataset buildBargraphDataset(Date fromDate, Date toDate) {
-        ((DefaultCategoryDataset) getDataset()).setValue((double)48/100, "Vegetables and Fruits", "Vegetables and Vegetable Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)48/100, "Vegetables and Fruits", "Fruits and Fruit Juices");
 
-        ((DefaultCategoryDataset) getDataset()).setValue((double)24/100, "Whole Grain Foods", "Breakfast Cereals");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)24/100, "Whole Grain Foods", "Cereals, Grains, and Pasta");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)24/100, "Whole Grain Foods", "Baked Products");
-
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Dairy and Egg Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Poultry Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Pork Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Nuts and Seeds");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Beef Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Finfish and Shellfish Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Legumes and Legume Products");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Proteins", "Lamb, Veal, and Game");
-
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Spices and Herbs");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Babyfoods");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Fats and Oils");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Soups, Sauces, and Gravies");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Sausages and Luncheon Meats");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Beverages");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Mixed Dishes");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Fast Foods");
-        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Others", "Sweets");
-
+        ((DefaultCategoryDataset) getDataset()).setValue((double)48/100, "Percentage", "Vegetables and Fruits");
+        ((DefaultCategoryDataset) getDataset()).setValue((double)24/100, "Percentage", "Whole Grain Foods");
+        ((DefaultCategoryDataset) getDataset()).setValue((double)23/100, "Percentage", "Proteins");
+        ((DefaultCategoryDataset) getDataset()).setValue((double)5/100, "Percentage", "Others");
 
 
 
@@ -80,6 +59,8 @@ public class CFGPlateVisualizer extends IVisualizer {
         ((DefaultPieDataset<String>) getDataset()).setValue("Proteins (23%)", 23.0);
         ((DefaultPieDataset<String>) getDataset()).setValue("Whole Grain Foods (24%)", 24.0);
         ((DefaultPieDataset<String>) getDataset()).setValue("Others (5%)", 5.0);
+
+
 
         return ((DefaultPieDataset<String>) getDataset());
     }
