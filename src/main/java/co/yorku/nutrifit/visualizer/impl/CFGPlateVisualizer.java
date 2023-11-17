5dadfbddc223413b4c0a32bbb209798ad60bee3d
Alexander Ukhin <alexanderukhin@gmail.com>
2023-11-16 21:23:57 -0500

    Comments for Visualizer

--- src/main/java/co/yorku/nutrifit/visualizer/impl/CFGPlateVisualizer.java
+++ src/main/java/co/yorku/nutrifit/visualizer/impl/CFGPlateVisualizer.java
@@ -11,8 +11,17 @@ import java.awt.*;
 import java.util.Date;
 import java.util.Map;
 
+/*
+ * The CFGPlateVisualizer class is designed to visualize data related to the user's plate and
+ * the CFG (Canada Food Guide). Uses JFreeChart to create bar graphs and pie charts to represent food group data.
+ *
+ * The class includes methods for building bar graph and pie chart datasets, as well as
+ * handling date range updates. It offers customization options for chart labels and expandability.
+ */
+
 public class CFGPlateVisualizer extends IVisualizer {
 
+    // Instance variable for CFGPlateCalculator
     private CFGPlateCalculator cfgPlateCalculator;
 
     public CFGPlateVisualizer(Dataset dataset, CFGPlateCalculator cfgPlateCalculator) {
@@ -27,38 +36,44 @@ public class CFGPlateVisualizer extends IVisualizer {
     }
 
     @Override
+    // Get the label for the x axis on the bar graph
     public String getBarGraphCategoryAxisLabel() {
         return "Food Categories";
     }
 
     @Override
+    // Get the label for the y axis on the bar graph
     public String getBarGraphValueAxisLabel() {
         return "Percentage";
     }
 
     @Override
+    // Checks if the bar graph should show percentages
     public boolean isBargraphShownInPercentage() {
         return true;
     }
 
     @Override
+    // Checks if the chart is expandable
     public boolean isChartExpandable() {
         return true;
     }
 
     @Override
+    // Method to build the dataset for bar graph
     public DefaultCategoryDataset buildBargraphDataset(String expandInfo, Date fromDate, Date toDate) {
-
+        // Get food group data from calculator
         Map<String, Double> data = cfgPlateCalculator.getFoodGroupData(expandInfo);
-
+        // Populate bar graph dataset
         for (Map.Entry<String, Double> stringIntegerEntry : data.entrySet()) {
             ((DefaultCategoryDataset) getDataset()).setValue(stringIntegerEntry.getValue(), "Food Group", stringIntegerEntry.getKey() + " (" + getDecimalFormat().format(stringIntegerEntry.getValue() * 100.0) + "%)");
         }
-
+        //Returns populated dataset
         return ((DefaultCategoryDataset) getDataset());
     }
 
     @Override
+    //Same as above but for pie chart
     public DefaultPieDataset<String> buildPiechartDataset(String expandInfo, Date fromDate, Date toDate) {
 
         Map<String, Double> data = cfgPlateCalculator.getFoodGroupData(expandInfo);
@@ -71,6 +86,7 @@ public class CFGPlateVisualizer extends IVisualizer {
     }
 
     @Override
+    // Method called when the date range is updated by user
     public void onDateRangeUpdate(String type, String expandData, Date newFromDate, Date newToDate) {
 
         if (!type.equals(this.getChartName())) return;
--- src/main/java/co/yorku/nutrifit/visualizer/impl/CalorieVisualizer.java
+++ src/main/java/co/yorku/nutrifit/visualizer/impl/CalorieVisualizer.java
@@ -9,8 +9,13 @@ import org.jfree.data.general.DefaultPieDataset;
 import java.util.Date;
 import java.util.Map;
 
-public class CalorieVisualizer extends IVisualizer {
+/*
+CalorieVisualizer is designed to visualize calorie consumption data. Utilizes JFreeChart library to create
+bar graphs and pie charts. The class implements a method to update the visualizer when the user modifies the date range.
+ */
 
+public class CalorieVisualizer extends IVisualizer {
+    // Instance variable for calorieCalculator
     private CalorieCalculator calorieCalculator;
 
     public CalorieVisualizer(CalorieCalculator calorieCalculator, Dataset dataset) {
@@ -24,28 +29,32 @@ public class CalorieVisualizer extends IVisualizer {
     }
 
     @Override
+    // Get the label for the x axis on the bar graph
     public String getBarGraphCategoryAxisLabel() {
         return "Date";
     }
 
     @Override
+    // Get the label for the y axis on the bar graph
     public String getBarGraphValueAxisLabel() {
         return "Calories Consumed";
     }
 
     @Override
+    // Method to build the dataset for bar graph
     public DefaultCategoryDataset buildBargraphDataset(String expandInfo, Date fromDate, Date toDate) {
-
+        // Get food group data from calculator
         Map<String, Integer> calorieData = calorieCalculator.getCaloriesPerDay(fromDate, toDate);
-
+        // Populate bar graph dataset
         for (Map.Entry<String, Integer> stringIntegerEntry : calorieData.entrySet()) {
             ((DefaultCategoryDataset) getDataset()).setValue(stringIntegerEntry.getValue(), "Calories Consumed", stringIntegerEntry.getKey());
         }
-
+        //Returns populated dataset
         return ((DefaultCategoryDataset) getDataset());
     }
 
     @Override
+    //Same as above but for pie chart
     public DefaultPieDataset<String> buildPiechartDataset(String expandInfo, Date fromDate, Date toDate) {
 
         Map<String, Integer> calorieData = calorieCalculator.getCaloriesPerDay(fromDate, toDate);
@@ -59,6 +68,7 @@ public class CalorieVisualizer extends IVisualizer {
     }
 
     @Override
+    // Method called when the date range is updated by user
     public void onDateRangeUpdate(String type, String expandData, Date newFromDate, Date newToDate) {
 
         if (!type.equals(this.getChartName())) return;
